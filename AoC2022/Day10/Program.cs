311edac6072ee999013c0c8b80759581df7e13b2
Jarosław Juranek <juranek91@gmail.com>
2022-12-11 21:36:03 +0100

    Day 10 code refactor

--- AoC2022/Day10/Program.cs
+++ AoC2022/Day10/Program.cs
@@ -1,8 +1,9 @@
-﻿using System.Threading.Channels;
-using Day10;
+﻿using Day10;
 
 var displaySignal = File.ReadAllLines("../../../DisplaySignal.txt");
 
+const int crtHeight = 6;
+const int crtWidth = 40;
 var registerX = 1;
 var cycleCounter = 0;
 var crt = new List<char>(240);
@@ -17,9 +18,9 @@ foreach (var command in displaySignal)
 
     for (int i = 0; i < parsedCommand.CycleDuration; i++)
     {
-        var isSpriteOverPixel = registerX == cycleCounter % 40
-                                || registerX - 1 == cycleCounter % 40
-                                || registerX + 1 == cycleCounter % 40;
+        var isSpriteOverPixel = registerX == cycleCounter % crtWidth
+                                || registerX - 1 == cycleCounter % crtWidth
+                                || registerX + 1 == cycleCounter % crtWidth;
         
         crt.Add(isSpriteOverPixel ? '#' : '.');
         cycleCounter++;
@@ -30,17 +31,14 @@ foreach (var command in displaySignal)
         //     signalSamples.Add(cycleCounter, cycleCounter * registerX);
         // }
     }
-
     registerX += parsedCommand.AddValue;
 }
 
 // Printing to Img
 var tmpImage = string.Join("", crt);
-var crtHeight = 6;
-for (int i = 0; i < crtHeight; i++)
+for (var i = 0; i < crtHeight; i++)
 {
-    finalImage += tmpImage.Substring(i * 40, 40) + '\n';
+    finalImage += string.Concat(tmpImage.AsSpan(i * crtWidth, crtWidth), "\n");
 }
-
 
 Console.WriteLine(finalImage);
